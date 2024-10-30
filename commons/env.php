5ecca9e6378beed80c9779a00e915fa78c5a3d7f
Thu Hoai <maithihoaithu0@gmail.com>
2024-10-30 22:24:55 +0700

    first commit

---
+++ commons/env.php
@@ -0,0 +1,14 @@
+<?php 
+
+// Biến môi trường, dùng chung toàn hệ thống
+// Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
+
+define('BASE_URL'   , 'http://localhost/DA1/');
+
+define('DB_HOST'    , 'localhost');
+define('DB_PORT'    , 3306);
+define('DB_NAME'    , 'thetrend');  // Tên database
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+
+define('PATH_ROOT'    , __DIR__ . '/../');
